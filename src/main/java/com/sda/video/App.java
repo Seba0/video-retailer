4dc89651de1b72d0a75146ef3d0fc6b538e20fce
Seba_0 <31700923+Seba0@users.noreply.github.com>
2018-08-12 13:36:31 +0200

    Initial commit

---
+++ src/main/java/com/sda/video/App.java
@@ -0,0 +1,13 @@
+package com.sda.video;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
